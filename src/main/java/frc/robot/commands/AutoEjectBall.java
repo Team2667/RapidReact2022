af6e3feeccffc7837fc6d8f0cbd28a403e14920f
kentbraaten <kentbraaten@frontiernet.net>
2022-11-17 20:54:54 -0600

    HAAAAAAAAAAANDS

--- src/main/java/frc/robot/commands/AutoEjectBall.java
+++ src/main/java/frc/robot/commands/AutoEjectBall.java
@@ -1,25 +1,34 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.BallGrabber;
 import frc.robot.subsystems.Belts;
 
 public class AutoEjectBall extends CommandBase {
     private Belts belt;
-    public AutoEjectBall(Belts belt)
+    private BallGrabber ballgrabber;
+    public AutoEjectBall(Belts belt,BallGrabber ballgrabber)
     {
         this.belt=belt;
+        this.ballgrabber=ballgrabber;
         this.addRequirements(belt);
+        this.addRequirements(ballgrabber);
     }
     @Override
     public void initialize()
     {
         belt.setBeltSpeed(1);
+        ballgrabber.forward();
     }
     @Override
     public void end(boolean interrupted)
     {
         if(interrupted)
+        {
             belt.stopBelt();
+            ballgrabber.stopGrabber();
+        }
+
     }
     
     @Override
